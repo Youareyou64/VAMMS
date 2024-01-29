02377145d61bd9ea72ba70113e8182ae83e08835
sjohnson1512 <154621049+sjohnson1512@users.noreply.github.com>
2024-01-29 09:28:53 -0600

    ok
    main edited and renderer drafted

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import math
 
 # Classes
 import graph
+import renderer
 
 raw_function = input("Enter your function: f(x,y)=")
 
+graph3D = graph
+graph.__init__(graph3D, raw_function)
 
-    
-     
+model = renderer
+renderer.__init__(model,graph3D.zArray)
+render(model)
---
+++ renderer.py
@@ -0,0 +1,8 @@
+
+
+class renderer:
+    def __init__(self, Arr2D):
+        self.Arr2D = Arr2D
+
+    def render(self):
+        return true
