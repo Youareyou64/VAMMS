d325a1b0c7042c243beea8a3cfac2da41926e8b7
Youareyou64 <71291558+Youareyou64@users.noreply.github.com>
2024-01-29 19:18:59 +0000

    possibly broke things with graph class

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import renderer
 raw_function = input("Enter your function: f(x,y)=")
 
 
-graph3D = graph
-graph.__init__(graph3D, raw_function)
+graph3D = graph(raw_function)
+graph3D.fill(0,5,0,5)
 
-model = renderer
-renderer.__init__(model,graph3D.zArray)
-render(model)
+#model = renderer
+#renderer.__init__(model,graph3D.zArray)
+#render(model)
 
