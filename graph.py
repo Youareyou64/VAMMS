Array successfully filled; just with incorrect values...

graph.py
@@ -1,36 +1,42 @@
 import math
 import cexprtk
+import matplotlib
+import numpy as np
 
 
 # set this value to number of discrete points in physical system
 global num_points_x, num_points_y 
 num_points_x = 10
 num_points_y = 10
+st = cexprtk.Symbol_Table({'x':1.0, 'y':1.0}, add_constants=True)
 
-class graph:
+class Graph:
     def __init__(self,funct):
         self.x_step = 1
         self.y_step = 1
-        self.parsed_function
+
         
         n = 5
         self.z_array = [[0]*num_points_x]*num_points_y
 
-        st = cexprtk.Symbol_Table({'x':1.0, 'y':1.0}, add_constants=True)
-        parsed_function = cexprtk.Expression(funct, st)
+
+        self.parsed_function = cexprtk.Expression(funct, st)
         # establish function as an Expression that can be evaluated
 
-        def f(x,y):
-            st.variables['x', 'y'] = [x, y]
-            return parsed_function()
+    def f(self, x, y):
+        st.variables['x'] = x # possible source of errors
+        st.variables['y'] = y
+        return self.parsed_function()
 
-    def fill(x_min, x_max, y_min, y_max):
+    def fill(self, x_min, x_max, y_min, y_max):
         x_step = (x_max - x_min) / num_points_x
         y_step = (y_max - y_min) / num_points_y
 
-        for x in range(0,num_points_x,x_step):
-            for y in range(0,num_points_y,y_step):
-                z_array[x][y] = f(x,y)
+        for x in range(0, num_points_x, 1):
+            for y in range(0, num_points_y, 1):
+                self.z_array[x][y] = self.f(x_min + x * x_step, y_min + y * y_step)
+                print(f"Filled {x}, {y} with f of {x_min + x * x_step}, {y_min + y * y_step} = {self.f(x_min + x * x_step, y_min + y * y_step)}")
+        print(np.matrix(self.z_array))
 
 
 

main.py
@@ -1,15 +1,17 @@
 # Dependencies
 import math
+import numpy as np
 
 # Classes
-import graph
+from graph import Graph
 import renderer
 
 raw_function = input("Enter your function: f(x,y)=")
 
 
-graph3D = graph(raw_function)
-graph3D.fill(0,5,0,5)
+graph3D = Graph(raw_function)
+graph3D.fill(0, 10, 0, 10) # x max, x min, y max, y min
+print(np.matrix(graph3D.z_array))
 
 #model = renderer
 #renderer.__init__(model,graph3D.zArray)