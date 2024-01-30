26be2877908cb262502ff1bb86f8d528e575f466
Youareyou64 <jackasloan@gmail.com>
2024-01-29 21:23:14 -0600

    Scaling dependencies

--- graph.py
+++ graph.py
@@ -15,14 +15,12 @@ class Graph:
         self.x_step = 1
         self.y_step = 1
 
-        
-        n = 5
-        # self.z_array = [[0]*num_points_x]*num_points_y
         self.array = np.zeros((num_points_x, num_points_y))
-
 
         self.parsed_function = cexprtk.Expression(funct, st)
         # establish function as an Expression that can be evaluated
+
+        print("Graph instance instantiated :)")
 
     def f(self, x, y):
         st.variables['x'] = x # possible source of errors
@@ -40,6 +38,32 @@ class Graph:
                 # print(f"Filled {x}, {y} with f of {x_min + x * x_step}, {y_min + y * y_step} = {self.f(x_min + x * x_step, y_min + y * y_step)}")
                 # print(np.matrix(self.array))
         # print(np.matrix(self.array))
+
+    def get_max(self):
+        maximum = self.array[0, 0]
+        for x in range(0, num_points_x, 1):
+            for y in range(0, num_points_y, 1):
+                if self.array[x, y] > maximum:
+                    maximum = self.array[x, y]
+        print(f"Max value: {maximum} ")
+        return maximum
+
+    def get_min(self):
+        minimum = self.array[0, 0]
+        for x in range(0, num_points_x, 1):
+            for y in range(0, num_points_y, 1):
+                if self.array[x, y] < minimum:
+                    minimum = self.array[x, y]
+        print(f"Min value: {minimum}")
+        return minimum
+
+    def scale(self):
+        min = self.get_min()
+        max = self.get_max()
+
+        # Finish this to scale data to available Z height and set appropriate z=0 offset
+
+
 
 
 
--- main.py
+++ main.py
@@ -7,11 +7,23 @@ from graph import Graph
 import renderer
 
 raw_function = input("Enter your function: f(x,y)=")
+x_min = int(input("Minimum x value (inclusive): ")) or 0
+x_max = int(input("Maximum x value (exclusive): ")) or 10
+y_min = int(input("Minimum y value (inclusive): ")) or 0
+y_max = int(input("Maximum y value (exclusive): ")) or 10
 
 
-graph3D = Graph(raw_function)
-graph3D.fill(0, 10, 0, 10) # x max, x min, y max, y min
-print(np.matrix(graph3D.array))
+def generate_graph(function, x_min, x_max, y_min, y_max):
+    graph = Graph(function)
+    graph.fill(x_min, x_max, y_min, y_max)  # x max, x min, y max, y min
+
+
+generate_graph(raw_function, x_min, x_max, y_min, y_max)
+
+
+# graph3D = Graph(raw_function)
+# graph3D.fill(0, 10, 0, 10) # x max, x min, y max, y min
+# print(np.matrix(graph3D.array))
 
 #model = renderer
 #renderer.__init__(model,graph3D.zArray)
