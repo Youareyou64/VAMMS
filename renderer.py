8dd370529a156f86b49e0483e5031dedb2e574f8
Youareyou64 <71291558+Youareyou64@users.noreply.github.com>
2024-02-02 14:45:17 +0000

    Finished Render

--- renderer.py
+++ renderer.py
@@ -10,9 +10,19 @@ class Renderer:
         assert len(arr2D.shape) == 2
 
     def render(self):
-        # connection = Connection()
+        connection = Connection()
+
+        connection.send("G28") # Home all axes
+        
+        
         for x in range(config.x_points):
             for y in range(config.y_points):
                 z = self.arr2D[x, y]
+                # X and Y values will need to be compensated for physical dimensions
+                connection.send(f"G0 X{x} Y{y}") # Move XY gantry
+                
+                connection.send(f"Z{z}") # Move linear actuator as Z axis
+                
                 print(self.arr2D[x, y])
             print('\n')
+
