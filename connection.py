e58528cd6294699bf2b6909d9fa2205384be9f6e
Youareyou64 <jackasloan@gmail.com>
2024-02-08 09:12:13 -0600

    serial connection

--- connection.py
+++ connection.py
@@ -1,21 +1,27 @@
 import serial
 import time
 
-serial_path = ''
-baud_rate = 0000
+serial_path = 'COM3'
+baud_rate = 9600
 
 
 class Connection:
     def __init__(self):
         try:
             self.ser = serial.Serial(serial_path, baud_rate, timeout=1)
+            print("Connected")
         except Exception as e:
+
             print(f"Error encountered during connection: {e}")
             
     def send(self, data):
         try:
-            self.ser.write(data)
-        except:
-            print(f"Error encountered during GCode send: {data}")
+            self.ser.write(data.encode())
+            print(f"Sent command {data}")
+        except Exception as e:
+            print(f"Error: {e}")
+            # print(f"Error encountered during GCode send: {data}")
+
+
     
 
