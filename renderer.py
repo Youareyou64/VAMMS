601c44a1dc7df915f333840079678c3046a2e04d
Youareyou64 <71291558+Youareyou64@users.noreply.github.com>
2024-02-23 02:12:24 +0000

    fixed rebase issues

--- renderer.py
+++ renderer.py
@@ -20,7 +20,7 @@ class Renderer:
         
         for x in range(config.x_points):
             for y in range(config.y_points):
-<<<<<<< HEAD
+
                 z = self.arr2D[x, y]
                 # X and Y values will need to be compensated for physical dimensions by multiplying by total axis length (in mm)
                 connection.send(f"G0 X{x * config.x_length / config.x_points} Y{y * config.y_length / config.y_points}") # Move XY gantry
@@ -29,7 +29,7 @@ class Renderer:
                 connection.send("G0 Z0") # Return to z = 0
                 
                 # print(self.arr2D[x, y])
-=======
+
                 try:
                     z = self.arr2D[x, y]
                     # X and Y values will need to be compensated for physical dimensions by multiplying by total axis length (in mm)
@@ -42,6 +42,6 @@ class Renderer:
                 except KeyboardInterrupt:
                     connection.send("M18") # disable steppers on emergency exit
                     print(Colors.RED + "Emergency Stop Triggered" + Colors.RESET + " | Steppers Disabled")
->>>>>>> 64664d6 (Feedrate config and KeyInterrupt Error Handling)
+
             print('\n')
 
