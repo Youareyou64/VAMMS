8e913e967c5fdb726dcdcc89a736ac6488ebcacb
Youareyou64 <71291558+Youareyou64@users.noreply.github.com>
2024-02-23 01:58:48 +0000

    Feedrate config and KeyInterrupt Error Handling 2

--- config.py
+++ config.py
@@ -3,6 +3,14 @@ x_points = 5
 y_points = 5
 
 # Physical dimensions of the system in mm
+<<<<<<< HEAD
 x_length = 330
 y_length = 360
 z_height = 50
+=======
+x_length = 300
+y_length = 300
+
+# Speed multiplier (percentage)
+feedrate = 150
+>>>>>>> 64664d6 (Feedrate config and KeyInterrupt Error Handling)
--- renderer.py
+++ renderer.py
@@ -1,3 +1,4 @@
+from colors import Colors
 from connection import Connection
 import config
 import numpy as np
@@ -14,10 +15,12 @@ class Renderer:
 
         # connection.send("G28") # Home all axes - modify to only x + y?
         connection.send("M17") # Enable steppers
+        connection.send(f"M220 S{config.feedrate}")
         # connection.send("G0 Y2000")
         
         for x in range(config.x_points):
             for y in range(config.y_points):
+<<<<<<< HEAD
                 z = self.arr2D[x, y]
                 # X and Y values will need to be compensated for physical dimensions by multiplying by total axis length (in mm)
                 connection.send(f"G0 X{x * config.x_length / config.x_points} Y{y * config.y_length / config.y_points}") # Move XY gantry
@@ -26,5 +29,19 @@ class Renderer:
                 connection.send("G0 Z0") # Return to z = 0
                 
                 # print(self.arr2D[x, y])
+=======
+                try:
+                    z = self.arr2D[x, y]
+                    # X and Y values will need to be compensated for physical dimensions by multiplying by total axis length (in mm)
+                    connection.send(f"G0 X{x} Y{y}") # Move XY gantry
+                    
+                    connection.send(f"G0 Z{z}") # Move linear actuator as Z axis
+                    connection.send("G0 Z0") # Return to z = 0
+                    
+                    # print(self.arr2D[x, y])
+                except KeyboardInterrupt:
+                    connection.send("M18") # disable steppers on emergency exit
+                    print(Colors.RED + "Emergency Stop Triggered" + Colors.RESET + " | Steppers Disabled")
+>>>>>>> 64664d6 (Feedrate config and KeyInterrupt Error Handling)
             print('\n')
 
