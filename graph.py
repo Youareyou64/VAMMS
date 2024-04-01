b0de38825c31985b8f324bda84d463687a0aede8
Youareyou64 <71291558+Youareyou64@users.noreply.github.com>
2024-04-01 18:03:35 +0000

    Function cleaning

--- graph.py
+++ graph.py
@@ -63,7 +63,11 @@ class Graph:
         min = self.get_min()
         max = self.get_max()
         
-
+        if(min == max):
+            for x in range(num_points_x):
+                for y in range(num_points_y):
+                    self.scaled_array[x, y] = 0
+            
         # Finish this to scale data to available Z height and set appropriate z=0 offset
 
         for x in range(num_points_x):
--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 # Dependencies
+
 import math
 import numpy as np
 from connection import Connection
 from graph import Graph
 from renderer import Renderer
 import dance
+from colors import Colors
 
 
 raw_function = input("Enter your function: f(x,y)=")
 if(raw_function) == "SJM":
     dance.dance()
     raw_function = input("Enter your function: f(x,y)=")
-x_min = int(input("Minimum x value (inclusive): ")) or 0
-x_max = int(input("Maximum x value (exclusive): ")) or 10
-y_min = int(input("Minimum y value (inclusive): ")) or 0
-y_max = int(input("Maximum y value (exclusive): ")) or 10
+    
+try:
+    x_min = int(input("Minimum x value (inclusive): "))
+except ValueError:
+    x_min = 0
 
+try:
+    x_max = int(input("Maximum x value (exclusive): "))
+except ValueError:
+    x_max = 10
+
+try:
+    y_min = int(input("Minimum y value (inclusive): "))
+except ValueError:
+    y_min = 0
+    
+try:
+    y_max = int(input("Maximum y value (exclusive): "))
+except ValueError:
+    y_max = 10
+
+
+def clean_function(function):
+    function = function.replace(" ", "")
+    function = function.replace("xy", "x * y")
+    function = function.replace("yx", "x * y")
+    print(Colors.CYAN + f"Graphing: {function}" + Colors.RESET)
+
+    return function
 
 def generate_graph(function, x_min, x_max, y_min, y_max):
     graph = Graph(function)
@@ -26,8 +52,10 @@ def generate_graph(function, x_min, x_max, y_min, y_max):
     renderer.render()
     
 
+cleaned_function = clean_function(raw_function)
 
-generate_graph(raw_function, x_min, x_max, y_min, y_max)
+generate_graph(cleaned_function, x_min, x_max, y_min, y_max)
+
 
 
 # graph3D = Graph(raw_function)
